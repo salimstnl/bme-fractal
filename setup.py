6ca069198d31ed696c2910a97820e561b9ac394b
Stanley Salim <119546127+BerryStorm@users.noreply.github.com>
2022-12-02 23:42:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='fractalpackage',
+    version='0.1.0',
+    description='Individual Research Project on Fractal',
+    author='Stanley Salim',
+    author_email='stanley.salim@hotmail.com',
+    url='https://github.com/BerryStorm/fractal',
+    packages=['fractalpackage'],
+    py_modules=[''],
+    install_requires=[
+        'copy',
+        'itertools',
+        'math',
+        'matplotlib',
+        'numpy',
+        'sympy',
+    ],
+)
