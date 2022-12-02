6b0f127821b5117d0965d5bd8f8215df3861fcd5
Stanley Salim <119546127+BerryStorm@users.noreply.github.com>
2022-12-02 21:32:53 +0100

    Update __init__.py

--- mod/__init__.py
+++ mod/__init__.py
@@ -1,11 +1,1 @@
-import sys, os.path as path
-from inspect import getsourcefile
 
-parent_dir = path.dirname(path.dirname(path.abspath(getsourcefile(lambda: 0))))
-sys.path.append(path.join(parent_dir, 'RepositoryName'))
-
-# make sure to hack the sys.path
-# before importing your own modules
-
-from . import module
-from .module import *
