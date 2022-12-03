244ac037e408858494c1d6e77b340a901c7f81e6
Stanley Salim <119546127+BerryStorm@users.noreply.github.com>
2022-12-03 17:09:30 +0100

    Update __init__.py

--- fractalpackage/__init__.py
+++ fractalpackage/__init__.py
@@ -9,33 +9,25 @@ plt.style.use('seaborn-dark')
 
 
 class Fractal:
-    """
-    Class representing a self-similar set.
+    """Class representing a self-similar set.
     """
   
     def __init__(self, x, y):
-        """
-        Initialize a Fractal object.
+        """Initialize a Fractal object.
         
-        Parameters:
-            cylinder : List representing the cylinder(s) for the self-similar 
-            set.
-            ifs      : Tuple representing the iterated function systems of the 
-            self-similar set.
-            n        : Positive integer representing the level of iteration(s).
-            (1 by default)
-        """a
+        Keyword arguments:
+        x -- x-coodinates of both endpoints of the cylinders
+        y -- y-coodinates of both endpoints of the cylinders
+        """
         self.x = x
         self.y = y
         
         
     def iterate(self):
-        """
-        Increase the cylinder level by one.
+        """Increase the cylinder level by one.
         """
 
       
     def plot(self):
-        """
-        Plot the fractal at the given level.
+        """Plot the fractal at the given level.
         """
